70ca795ef48ef45a46d3534da30a05a02854e6c3
DESKTOP-NDCBFHM\MrUser <robert.kamenski.rld@gmail.com>
2025-01-02 14:05:09 +0100

    project init

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DR_Tic_Tac_Toe
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
